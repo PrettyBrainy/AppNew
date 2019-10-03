87faf01c5b5c34ba28e5d2cbc3a6a6fd07c788fc
PrettyBrainy <53316351+PrettyBrainy@users.noreply.github.com>
2019-10-03 10:08:52 -0600

    Add files via upload

---
+++ src/app/welcome/welcome4/welcome4.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome4',
+  templateUrl: './welcome4.page.html',
+  styleUrls: ['./welcome4.page.scss'],
+})
+export class Welcome4Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
