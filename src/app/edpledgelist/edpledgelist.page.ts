d81a16a88f49c5fea93a5c0228837a793bc95d79
PrettyBrainy <53316351+PrettyBrainy@users.noreply.github.com>
2019-10-13 13:39:54 -0600

    Add files via upload

--- src/app/edpledgelist/edpledgelist.page.ts
+++ src/app/edpledgelist/edpledgelist.page.ts
@@ -1,5 +1,7 @@
+
 import { EventService } from './../services/pledges/pledge-service.service';
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from './../services/user/profile.service';
 
 
 
@@ -10,8 +12,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EdpledgelistPage implements OnInit {
   public eventList: Array<any>;
-  public eventListRef: firebase.firestore.DocumentReference;
-  constructor(private eventService: EventService) {}
+  public eventListRef: firebase.firestore.CollectionReference;
+  public userProfile: any = {};
+  constructor(private eventService: EventService,
+              public profileService: ProfileService ) {}
 
   ngOnInit() {
 
@@ -28,9 +32,9 @@ export class EdpledgelistPage implements OnInit {
         });
         });
       });
-
     
-    this.eventService.pullEventListAdults().then( eventListSnapshot => {
+
+    this.eventService.pullEventListKids().then( eventListSnapshot => {
       this.eventList = [];
       eventListSnapshot.forEach(snap => {
         this.eventList.push({
@@ -40,18 +44,66 @@ export class EdpledgelistPage implements OnInit {
           pledge: snap.data().pledge,
           pledgeDetails: snap.data().pledgeDetails,
           learning: snap.data().learning
-        })
-        this.eventList.forEach(snap => {
-          this.eventService.createEvent(
-            snap.data().name, 
-            snap.data().impact, 
-            snap.data().learning, 
-            snap.data().pledge, 
-            snap.data().pledgeDetails);
-      });
+        }) 
+        console.log(this.eventList);
+        /*this.eventList.forEach(
+          this.eventService
+          .createEvent(
+            name, 
+            impact, 
+           learning, 
+            pledge, 
+            pledgeDetails);
+      );*/
       });
     });
   }
+
+
+  /*
+
+copyPledges(
+  id: string, 
+  name: string, 
+  impact: number,
+  pledge: string,
+  pledgeDetails: string,
+  learning: string): void{
+  
+   this.profileService.getUserProfile().then(userProfileSnapshot => {
+    var userAge = this.userProfile.data().birthDate;
+    if (userAge == "11-15"){
+      this.eventService.pullEventListKids().then( eventListSnapshot => {
+        this.eventList = [];
+        eventListSnapshot.forEach(snap => {
+          this.eventList.push({
+            id: snap.id,
+            name: snap.data().name,
+            impact: snap.data().impact,
+            pledge: snap.data().pledge,
+            pledgeDetails: snap.data().pledgeDetails,
+            learning: snap.data().learning}
+          ) 
+          this.eventList.forEach(this.eventService
+            .createEvent(snap.id,
+                         snap.data().name,
+                         snap.data().impact,
+                         snap.data().pledge,
+                         snap.data().pledgeDetails,
+                         snap.data().learning))
+          }
+        
+      )}
+      )}
+
+  }); 
+}*/
 }
+
+
+
+
+
+
 
 
