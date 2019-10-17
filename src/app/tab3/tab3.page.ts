e2e6f3a0493b4e7f6cd8e8219815f8c47cf4a29e
eyounger20 <34873699+eyounger20@users.noreply.github.com>
2019-10-16 18:29:02 -0600

    Update tab3.page.ts

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  
+  public businessName: string;
+  public suggestionId: string;
+  public hideBusinesses: boolean=false;
+  public moreBusinesses: boolean=true;
 
-  constructor() {}
+  constructor(private business: BusinessesService) {}
+  
+  getBusinesses(){
+    this.business.addBusiness(this.businessName, this.suggestionId );
+    this.hideBusinesses = true;
+    this.moreBusinesses= false;
+  }
+
+  anotherBusiness(){
+    this.moreBusinesses = true;
+    this.hideBusinesses = false;
+  }
 
 }
