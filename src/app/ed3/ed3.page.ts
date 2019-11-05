696d3afd8a14dbd3244e07d155396fd306d6d319
PrettyBrainy <53316351+PrettyBrainy@users.noreply.github.com>
2019-11-04 17:22:49 -0700

    Add files via upload

--- src/app/ed3/ed3.page.ts
+++ src/app/ed3/ed3.page.ts
@@ -1,3 +1,4 @@
+import { ProfileService } from './../services/user/profile.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ed3.page.scss'],
 })
 export class Ed3Page implements OnInit {
-
-  constructor() { }
+  public kidPledge: boolean = true;
+  public teenPledge: boolean = true;
+  public adultPledge: boolean = true;
+  constructor(
+    public profileService: ProfileService
+  ) {}
 
   ngOnInit() {
+    this.kidPledge = true;
+    this.teenPledge = true;
+    this.adultPledge = true;
+    this.profileService.getUserProfile().then((userProfileSnapshot) => {
+      if (userProfileSnapshot.data()) {
+        var userAge = String(userProfileSnapshot.data().birthDate);
+        if (userAge == "11-15"){
+          this.kidPledge = false;
+        }
+        else if( userAge == "16-18"){
+          this.teenPledge = false;
+        }
+        else if( userAge == "18-20" || userAge == "21+"){
+          this.adultPledge = false;
+        }
+      }
+  
+    }
+  )
   }
+
 
 }
--- src/app/ed5/ed5.page.ts
+++ src/app/ed5/ed5.page.ts
@@ -1,4 +1,6 @@
+import { ProfileService } from './../services/user/profile.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-ed5',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ed5Page implements OnInit {
 
-  constructor() { }
+  public kidPledge: boolean = true;
+  public teenPledge: boolean = true;
+  public adultPledge: boolean = true;
+  constructor(
+    public profileService: ProfileService
+  ) {}
 
   ngOnInit() {
+    this.kidPledge = true;
+    this.teenPledge = true;
+    this.adultPledge = true;
+    this.profileService.getUserProfile().then((userProfileSnapshot) => {
+      if (userProfileSnapshot.data()) {
+        var userAge = String(userProfileSnapshot.data().birthDate);
+        if (userAge == "11-15"){
+          this.kidPledge = false;
+        }
+        else if( userAge == "16-18"){
+          this.teenPledge = false;
+        }
+        else if( userAge == "18-20" || userAge == "21+"){
+          this.adultPledge = false;
+        }
+      }
+  
+    }
+  )
   }
+
 
 }
