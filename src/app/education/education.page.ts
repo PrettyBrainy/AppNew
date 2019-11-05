37581ea91457671026138b180a9c65d242cc09de
PrettyBrainy <53316351+PrettyBrainy@users.noreply.github.com>
2019-11-04 17:54:24 -0700

    Add files via upload

--- src/app/education/education.page.ts
+++ src/app/education/education.page.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationPage implements OnInit {
   x:number;
+
   constructor() { this.x=45 }
 
   ngOnInit() {
   }
+  
+public hideButton: boolean=false;
+public hideText: boolean=false;
+public hidePledge1: boolean=true;
+public hidePledge2: boolean=true;
+public hidePledge3: boolean=true;
+public hidePledge4: boolean=true;
+public hidePledge5: boolean=true;
+public hidePledges: boolean=true;
+
+
+startEdModule(){
+  this.hideButton=true;
+  this.hideText=true;
+  this.hidePledges=false;
+  this.hidePledge1=false;
+  this.hidePledge2=false;
+  this.hidePledge3=false;
+  this.hidePledge4=false;
+  this.hidePledge5=false;
+
+}
+
 
 }
