b0b36714f20410ffd9fc4762c75a4a97b0babc15
PrettyBrainy <53316351+PrettyBrainy@users.noreply.github.com>
2019-11-18 18:32:03 -0700

    Add files via upload

--- src/app/services/pledges/verify-service.service.ts
+++ src/app/services/pledges/verify-service.service.ts
@@ -1,6 +1,10 @@
 import { Router } from '@angular/router';
 import { AuthService } from './../user/auth.service';
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
 
 
 @Injectable({
@@ -10,14 +14,24 @@ export class VerifyServiceService {
 
   constructor(private authService: AuthService,
               private router: Router) { }
+              public createPledgeList: firebase.firestore.DocumentReference;
 
 getCurrent(){
   let view = this.router.url;
   let id = view.substr(1);
-  console.log("This page is called " + id);
 }
 
-verifyText( id: string){
+verifyText(verf: string){
+const id = this.getCurrent();
+var user = firebase.auth().currentUser;
+var uid;
+if (user != null) {
+  uid = user.uid; 
+}
+
+this.createPledgeList = firebase.firestore().collection('userProfile').doc(`${uid}`);
+
+
 
 }
 
