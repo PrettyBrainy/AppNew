5afb45874aeb7bfddbd1de7f157279abe7802575
PrettyBrainy <53316351+PrettyBrainy@users.noreply.github.com>
2019-12-15 15:19:03 -0700

    Update profile.page.ts

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,8 +1,13 @@
+import { Tab1PageModule } from './../tab1/tab1.module';
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { AuthService } from './../services/user/auth.service';
 import { ProfileService } from './../services/user/profile.service';
 import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
  
 @Component({
   selector: 'app-profile',
@@ -13,12 +18,16 @@ export class ProfilePage implements OnInit {
   public userProfile: any;
   public birthDate: Date;
   public age: boolean = false;
+  public ageRange: boolean = false;
   public drop: boolean = false;
- public ageRange: boolean = true;
   public lAge: boolean=true;
   public mAge: boolean=true;
   public hAge: boolean=true;
   public oAge: boolean=true;
+  public array: any;
+  public team: string;
+  public showTeam: boolean = false;
+  public showTeamButtons: boolean = true;
   constructor(
     private alertCtrl: AlertController,
     private authService: AuthService,
@@ -34,8 +43,8 @@ export class ProfilePage implements OnInit {
         this.userProfile = userProfileSnapshot.data();
       }
     });
-   
-       this.profileService.getUserProfile().then((userProfileSnapshot) => {
+
+    this.profileService.getUserProfile().then((userProfileSnapshot) => {
       if (userProfileSnapshot.data()) {
         var userAge = String(userProfileSnapshot.data().birthDate);
         if (userAge == "11-15" || "16-18" || "18-20" || "21+"){
@@ -53,7 +62,17 @@ export class ProfilePage implements OnInit {
     }
     )
 
+  //_______________________Check For Teams
+  var teamCheck = this.checkForTeam();
+  if (teamCheck!=undefined){
+    this.showTeam = true;
+    this.showTeamButtons = false;
+  } else {
+    this.showTeam = false;
+    this.showTeamButtons = true;
   }
+ 
+}
 
   logOut(): void {
     this.authService.logoutUser().then( () => {
@@ -98,6 +117,7 @@ export class ProfilePage implements OnInit {
     }
     this.profileService.updateDOB(birthDate);
   }
+
 
   async updateEmail(): Promise<void> {
     const alert = await this.alertCtrl.create({
@@ -150,7 +170,6 @@ export class ProfilePage implements OnInit {
 lowAge(){
   this.age = true;
   this.drop = true;
-  this.ageRange = false;
   this.lAge = false;
   this.mAge = true;
   this.hAge = true;
@@ -161,7 +180,6 @@ lowAge(){
 midAge(){
   this.age = true;
   this.drop = true;
-  this.ageRange = false;
   this.mAge = false;
   this.lAge = true;
   this.hAge = true;
@@ -172,7 +190,6 @@ midAge(){
 highAge(){
   this.age = true;
   this.drop = true;
-  this.ageRange = false;
   this.hAge = false;
   this.lAge =  true;
   this.mAge = true;
@@ -183,11 +200,37 @@ highAge(){
 oldAge(){
   this.age = true;
   this.drop = true;
-  this.ageRange = false;
   this.hAge = true;
   this.lAge =  true;
   this.mAge = true;
   this.oAge = false;
   this.updateDOB('21+');
 }
+
+createTeam(teamId: string, accessCode: string) {
+
+  const teamRef = firebase.firestore().collection('teams').doc(`${teamId}`)
+  const teamsList = firebase.firestore().collection('teams')
+  teamRef.get()
+    .then((docSnapshot) => {
+      if (docSnapshot.exists) {
+        alert('user ' + teamId + ' exists!');
+      } else {
+      var teamName = teamId;
+      var accessCode = accessCode;
+      teamsList.doc(teamName).set(accessCode)
+      }
+      });
+  
+  }
+checkForTeam(){
+  this.profileService.getUserProfile().then((userProfileSnapshot) => {
+    if (userProfileSnapshot.data()) {
+      this.team = String(userProfileSnapshot.data().team)
+    }
+  })
+return this.team;}
+
+
+
 }
