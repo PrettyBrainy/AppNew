ba94b50b1db01792160cc2d729308f9436805635
eyounger20 <34873699+eyounger20@users.noreply.github.com>
2019-12-17 16:41:41 -0700

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ProfileService } from './../services/user/profile.service';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
 
-  constructor() {}
+export class Tab1Page implements OnInit{
+  x:number;
+  public userPoints:number;
+  public userProfile: any;
+  constructor(private profileService: ProfileService) 
+  { this.x=45 }
 
+
+ngOnInit() {
+  this.profileService.getUserProfile().then((userProfileSnapshot) => {
+    if (userProfileSnapshot.data()) {
+      var userPoints = Number(userProfileSnapshot.data().points);
+    console.log(userPoints); 
+    }
+  })
+  }
 }
+
+
