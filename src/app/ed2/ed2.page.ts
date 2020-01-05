e310563296c1d50dbdea0c779217c5afbab4cdaa
PrettyBrainy <53316351+PrettyBrainy@users.noreply.github.com>
2020-01-05 14:51:13 -0700

    Update ed2.page.ts

--- src/app/ed2/ed2.page.ts
+++ src/app/ed2/ed2.page.ts
@@ -1,142 +1,220 @@
-import { Router } from '@angular/router';
-import { VerifyServiceService } from './../services/pledges/verify-service.service';
-import { ProfileService } from './../services/user/profile.service';
+  
+import { EventService } from './../services/pledges/pledge-service.service';
+import { AuthService } from '../services/user/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { Plugins, CameraResultType } from '@capacitor/core';
-const { Camera } = Plugins;
+import { ProfileService } from './../services/user/profile.service';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
 
 @Component({
-  selector: 'app-ed2',
-  templateUrl: './ed2.page.html',
-  styleUrls: ['./ed2.page.scss'],
+  selector: 'app-education',
+  templateUrl: './education.page.html',
+  styleUrls: ['./education.page.scss'],
 })
-export class Ed2Page implements OnInit {
-  public kidPledge: boolean = true;
-  public teenPledge: boolean = true;
-  public adultPledge: boolean = true;
-  public pledgePicture: string = null;
-  public hideVerfCard: boolean = false;
-  public verification = '';
-  public id = '';
-  public pledgeVerificationUpdate: firebase.firestore.DocumentReference;
-  
-  constructor(
-    public profileService: ProfileService,
-    public verifyService: VerifyServiceService,
-    public router: Router
-
-  ) {}
+export class EducationPage implements OnInit {
+x:number;
+public hideButton: boolean=false;
+public hideText: boolean=false;
+public incompletePledge1: boolean=true;
+public incompletePledge2: boolean=true;
+public incompletePledge3: boolean=true;
+public incompletePledge4: boolean=true;
+public incompletePledge5: boolean=true;
+public hidePledges: boolean=true;
+public completePledge1: boolean=false;
+public completePledge2: boolean=false;
+public completePledge3: boolean=false;
+public completePledge4: boolean=false;
+public completePledge5: boolean=false;
+public createPledgeList: firebase.firestore.DocumentReference;
+public pledgeListRef: firebase.firestore.CollectionReference;
+public array: Array<any>;
+private uid: String;
+public complete: boolean;
+public checkComplete: String;
+public edPledges: firebase.firestore.DocumentReference;
+public pledgeContent: String;
+public pledgeCount: String;
+  constructor(private authService: AuthService,
+              private profileService: ProfileService,
+              private eventService: EventService) { this.x=45 }
 
   ngOnInit() {
-    this.kidPledge = true;
-    this.teenPledge = true;
-    this.adultPledge = true;
-    this.profileService.getUserProfile().then((userProfileSnapshot) => {
-      if (userProfileSnapshot.data()) {
-        var userAge = String(userProfileSnapshot.data().birthDate);
-        if (userAge == "11-15"){
-          this.kidPledge = false;
-        }
-        else if( userAge == "16-18"){
-          this.teenPledge = false;
-        }
-        else if( userAge == "18-20" || userAge == "21+"){
-          this.adultPledge = false;
-        }
-      }
-  
+    //____________________Import userId
+    var user = firebase.auth().currentUser;
+    var uid;
+    if (user != null) {
+      uid = user.uid; 
+      this.uid = user.uid;
     }
-  )
+
+
+//______________________________Check if mod started using exist funcion
+let modStarted = firebase.firestore()
+    .collection('userProfile')
+    .doc(`${uid}`)
+    .collection('pledges')
+    .doc('education')
+    .get().then((docSnapshot)=>{
+      if(docSnapshot.exists){
+        this.hideButton=true;
+        this.hideText=true;
+        this.hidePledges=false;
+      } else{
+        this.hideButton=false;
+        this.hideText=false;
+        this.hidePledges=true;
+      }
+})
+
+var count = 0;
+//__________________________________________ Show/Hide for all ed pledges
+let data1 = firebase.firestore().collection('userProfile').doc(`${uid}`).collection('pledges').doc('education').get()
+.then((docSnapshot) =>{
+  var ed1Verf = String(docSnapshot.data().ed1); 
+  console.log(ed1Verf)
+  if (ed1Verf == ' '){
+    this.incompletePledge1=false;
+    this.completePledge1=true;
+    console.log("no data - 1");
+   
+  }
+  else{
+    this.incompletePledge1=true;
+    this.completePledge1=false;
+    console.log("has data - 1");
+    count += 1;
   }
 
-getCurrent(){
-  let view = this.router.url;
-  let id = view.substr(1);
-  return id;
-} 
-
-
-async takePicture(): Promise<void> {
-  try {
-    const profilePicture = await Camera.getPhoto({
-      quality: 90,
-      allowEditing: false,
-      resultType: CameraResultType.Base64
-    });
-    this.pledgePicture = profilePicture.base64String;
-  } catch (error) {
-    console.error(error);
+  var ed2Verf = String(docSnapshot.data().ed2); 
+  console.log(ed2Verf)
+  if (ed2Verf == ' '){
+    this.incompletePledge2=false;
+    this.completePledge2=true;
+    console.log("no data - 2");
   }
+  else{
+    this.incompletePledge2=true;
+    this.completePledge2=false;
+    console.log("has data - 2");
+    count += 1;
+  }
+
+  var ed3Verf = String(docSnapshot.data().ed3); 
+  console.log(ed3Verf)
+  if (ed3Verf == ' '){
+    this.incompletePledge3=false;
+    this.completePledge3=true;
+    console.log("no data - 3");
+  }
+  else{
+    this.incompletePledge3=true;
+    this.completePledge3=false;
+    console.log("has data - 3");
+    count += 1;
+  }
+
+  var ed4Verf = String(docSnapshot.data().ed4); 
+  console.log(ed4Verf)
+  if (ed4Verf == ' '){
+    this.incompletePledge4=false;
+    this.completePledge4=true;
+    console.log("no data - 4");
+  }
+  else{
+    this.incompletePledge4=true;
+    this.completePledge4=false;
+    console.log("has data - 4");
+    count += 1;
+  }
+
+  var ed5Verf = String(docSnapshot.data().ed5); 
+  console.log(ed5Verf)
+  if (ed5Verf == ' '){
+    this.incompletePledge5=false;
+    this.completePledge5=true;
+    console.log("no data - 5");
+  }
+  else{
+    this.incompletePledge5=true;
+    this.completePledge5=false;
+    console.log("has data - 5");
+    count += 1;
+  }
+var countPercent = (count/5)*100;
+this.pledgeCount = String(countPercent);
+console.log(this.pledgeCount);
+}) 
+
+}
+  
+
+startEdModule(){
+  this.hideButton=true;
+  this.hideText=true;
+  this.hidePledges=false;
+  this.incompletePledge1=false;
+  this.incompletePledge2=false;
+  this.incompletePledge3=false;
+  this.incompletePledge4=false;
+  this.incompletePledge5=false;
+  
+  this.createPledgeList = firebase.firestore().collection('userProfile').doc(`${this.uid}`);
+  let pledges = {
+    ed1: " ",
+    ed2: " ",
+    ed3: " ",
+    ed4: " ",
+    ed5: " ",
+  }
+  this.createPledgeList.collection("pledges").doc("education").set(pledges);
+  
 }
 
 
-verifyPledge(){
-  this.profileService.getUserProfile().then((userProfileSnapshot) => {
-      var team = String(userProfileSnapshot.data().team);
-      var indiPoints = Number(userProfileSnapshot.data().points)
-    
-  //_____________________________________________Call User ID
-  var user = firebase.auth().currentUser;
-  var uid;
-  if (user != null) {
-    uid = user.uid; 
-  }
-  //__________________________________________Assign user input to variable
-  let verificationUpdate = {
-    ed2: this.verification
-  }
-
-  //_________________________Get Team Info and Update Team Points and Update Team Pledge Progress Count_________________
-  const teamRef = firebase.firestore().collection("teams").doc(`${team}`);
-  let getTeam = teamRef.get().then(doc =>{
-      var teamPoints = doc.data().points;
-      let newTeamPoints = Number(teamPoints) + 100;   //Update with correct points
-
-      var teamPledgeCount = doc.data().edPledgeComplete;
-      let newTeamPledgeCount = Number(teamPledgeCount) + 1;
-
-      let newInfoTeam = {
-        points: `${newTeamPoints}`,
-        edPledgeComplete: `${newTeamPledgeCount}`
-        }
-      let updateTeamInfo = teamRef.update(newInfoTeam);
-  })
-
-  //_________________________Get City Info and Update City Points_________________
-  const cityRef = firebase.firestore().collection("cityOverall").doc("cityOverall");
-  let getCity = cityRef.get().then(doc => {
-      var cityPoints = doc.data().points;
-      let newCityPoints = Number(cityPoints) + 100;    //Update with correct points
-      
-      var cityPledgeCount = doc.data().edPledgeComplete;
-      let newCityPledgeCount = Number(cityPledgeCount) + 1;
-      
-      
-      let newInfoCity = {
-        points: `${newCityPoints}`,
-        edPledgeComplete: `${newCityPledgeCount}`
-      }
-      let updateCityInfo = cityRef.update(newInfoCity);
-  })
-
-  //_________________________Get User Info and Update User Points_________________
-  const userRef = firebase.firestore().collection('userProfile').doc(`${uid}`);
-  let getUser = userRef.get().then(doc=>{
-      var userPoints = doc.data().points;
-      let newIndiPoints = Number(indiPoints) + 100;   //Update with correct points
-      let newPointsIndi = {
-        points: `${newIndiPoints}`
-      }
-      let updateIndiPoints = userRef.update(newPointsIndi);
-  })
-  //__________________________________________Update Database with verification
-  userRef.collection("pledges").doc("education").update(verificationUpdate);
-  })
-this.hideVerfCard = true;
+checkForCompletion(pledge: String){
+this.checkComplete = String(this.retreiveContent(pledge));
+    console.log(this.checkComplete)
+    if (this.checkComplete == ' '){
+      this.complete=true;
+      console.log("no data - 5");
+      return this.complete;
+    }
+    else{
+      this.complete = false;
+      console.log("has data - 5");
+      return this.complete;
+    }
 }
+
+
+retreiveContent(pledge:String){
+let bumblebee = firebase.firestore().collection('userProfile').doc(`${this.uid}`).collection('pledges').doc('education').get().then((docSnapshot)=>{
+  switch (pledge) {
+  case 'ed1':
+    this.pledgeContent = String(docSnapshot.data().ed1);
+    return this.pledgeContent;
+  case 'ed2':
+    this.pledgeContent = String(docSnapshot.data().ed2);
+    return this.pledgeContent;
+  case 'ed3':
+    this.pledgeContent = String(docSnapshot.data().ed3);
+    return this.pledgeContent;
+  case 'ed4':
+    this.pledgeContent = String(docSnapshot.data().ed4);
+    return this.pledgeContent;
+  case 'ed5':
+    this.pledgeContent = String(docSnapshot.data().ed5);
+    return this.pledgeContent;
+  default:
+    break;
+}
+})
+}
+
 
 }
