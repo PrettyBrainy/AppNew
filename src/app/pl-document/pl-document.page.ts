f34d98c9ee5a54634dce8b585e739f9a53acfaea
eyounger20 <34873699+eyounger20@users.noreply.github.com>
2020-03-30 12:41:26 -0600

    Add files via upload

---
+++ src/app/pl-document/pl-document.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pl-document',
+  templateUrl: './pl-document.page.html',
+  styleUrls: ['./pl-document.page.scss'],
+})
+export class PlDocumentPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
