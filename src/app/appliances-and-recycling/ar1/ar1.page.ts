4875f2d02b29287744da9594810587d309b746aa
madzieboyles <53319246+madzieboyles@users.noreply.github.com>
2022-01-31 21:39:04 -0700

    Update ar1.page.ts

--- src/app/appliances-and-recycling/ar1/ar1.page.ts
+++ src/app/appliances-and-recycling/ar1/ar1.page.ts
@@ -137,14 +137,31 @@ checkForPledgeContent(){
   if (user != null) {
     this.uid = user.uid;
   }
-let verfCheck = firebase.firestore().collection('userProfile').doc(`${this.uid}`).collection('pledges').doc('appliancesAndRecycling').get().then((docSnapshot)=>{
-  this.pledgeContent = docSnapshot.data().ar1;
+let verfCheck = firebase.firestore().collection('userProfile').doc(`${this.uid}`).collection('pledges').doc('education').get().then((docSnapshot)=>{
+  this.pledgeContent = docSnapshot.data().ed2;
   console.log(this.pledgeContent);
 
-  if (this.pledgeContent !=''){
+  //NEW CONTENT BELOW HERE
+  if (this.pledgeContent.includes(' ') || this.pledgeContent.includes('.') || this.pledgeContent.includes(',')) {
+    let approvalStatus={
+      ar1: "approved"
+    }
     this.hideVerfCard = true;
+    const approved = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
+    .collection('approval').doc('appliancesAndRecycling').update(approvalStatus);
+    this.pledgeSubmittedCard = false;
+  }
+  if (this.pledgeContent !=''){
+    let approvalStatus = {
+      ar1: "pending"
+    }
+    this.hideVerfCard = true;
+    const pending = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
+    .collection('approval').doc('appliancesAndRecycling').update(approvalStatus);
+    this.pledgeSubmittedCard = false;
   }
 })
+// NEW CONTENT ABOVE HERE
 this.checkForPledgeStatus();
 }
 
