cfb04122b00f57ba9871abf380f37ae4c5efc268
madzieboyles <53319246+madzieboyles@users.noreply.github.com>
2022-01-31 21:45:12 -0700

    Update ed3.page.ts

--- src/app/ed3/ed3.page.ts
+++ src/app/ed3/ed3.page.ts
@@ -150,13 +150,30 @@ checkForPledgeContent(){
     this.uid = user.uid;
   }
 let verfCheck = firebase.firestore().collection('userProfile').doc(`${this.uid}`).collection('pledges').doc('education').get().then((docSnapshot)=>{
-  this.pledgeContent = docSnapshot.data().ed3;
+  this.pledgeContent = docSnapshot.data().ed2;
   console.log(this.pledgeContent);
 
-  if (this.pledgeContent !=''){
+  //NEW CONTENT BELOW HERE
+  if (this.pledgeContent.includes(' ') || this.pledgeContent.includes('.') || this.pledgeContent.includes(',')) {
+    let approvalStatus={
+      ed3: "approved"
+    }
     this.hideVerfCard = true;
+    const approved = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
+    .collection('approval').doc('appliancesAndRecycling').update(approvalStatus);
+    this.pledgeSubmittedCard = false;
+  }
+  if (this.pledgeContent !=''){
+    let approvalStatus = {
+      ed3: "pending"
+    }
+    this.hideVerfCard = true;
+    const pending = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
+    .collection('approval').doc('education').update(approvalStatus);
+    this.pledgeSubmittedCard = false;
   }
 })
+// NEW CONTENT ABOVE HERE
 this.checkForPledgeStatus();
 }
 
