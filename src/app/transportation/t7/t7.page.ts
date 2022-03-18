50dba3c8299822ef742b3386f95f37d4155f0a3d
madzieboyles <53319246+madzieboyles@users.noreply.github.com>
2022-03-18 16:20:07 -0600

    Update t7.page.ts

--- src/app/transportation/t7/t7.page.ts
+++ src/app/transportation/t7/t7.page.ts
@@ -131,7 +131,6 @@ this.pledgeSubmittedCard = false;
 }
 
 
-
 checkForPledgeContent(){
   var user = firebase.auth().currentUser;
   if (user != null) {
@@ -141,29 +140,13 @@ let verfCheck = firebase.firestore().collection('userProfile').doc(`${this.uid}`
   this.pledgeContent = docSnapshot.data().t7;
   console.log(this.pledgeContent);
 
-  //NEW CONTENT BELOW HERE
-  if (this.pledgeContent.includes(' ') || this.pledgeContent.includes('.') || this.pledgeContent.includes(',')) {
-    let approvalStatus={
-      t7: "approved"
-    }
-    this.hideVerfCard = true;
-    const approved = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
-    .collection('approval').doc('transportation').update(approvalStatus);
-    this.pledgeSubmittedCard = false;
-  }
   if (this.pledgeContent !=''){
-    let approvalStatus = {
-      t7: "pending"
-    }
     this.hideVerfCard = true;
-    const pending = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
-    .collection('approval').doc('transportation').update(approvalStatus);
-    this.pledgeSubmittedCard = false;
   }
 })
-// NEW CONTENT ABOVE HERE
 this.checkForPledgeStatus();
 }
+
 
 
 checkForPledgeStatus(){
