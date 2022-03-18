82ab11f08b860f0269c3a4d92cbe1bcc3464b125
madzieboyles <53319246+madzieboyles@users.noreply.github.com>
2022-03-18 15:47:13 -0600

    Update ed5.page.ts

--- src/app/ed5/ed5.page.ts
+++ src/app/ed5/ed5.page.ts
@@ -136,7 +136,6 @@ this.pledgeSubmittedCard = false;
 }
 
 
-
 checkForPledgeContent(){
   var user = firebase.auth().currentUser;
   if (user != null) {
@@ -146,27 +145,10 @@ let verfCheck = firebase.firestore().collection('userProfile').doc(`${this.uid}`
   this.pledgeContent = docSnapshot.data().ed5;
   console.log(this.pledgeContent);
 
-  //NEW CONTENT BELOW HERE
-  if (this.pledgeContent.includes(' ') || this.pledgeContent.includes('.') || this.pledgeContent.includes(',')) {
-    let approvalStatus={
-      ed5: "approved"
-    }
-    this.hideVerfCard = true;
-    const approved = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
-    .collection('approval').doc('education').update(approvalStatus);
-    this.pledgeSubmittedCard = false;
-  }
   if (this.pledgeContent !=''){
-    let approvalStatus = {
-      ed5: "pending"
-    }
     this.hideVerfCard = true;
-    const pending = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
-    .collection('approval').doc('education').update(approvalStatus);
-    this.pledgeSubmittedCard = false;
   }
 })
-// NEW CONTENT ABOVE HERE
 this.checkForPledgeStatus();
 }
 
