1033b25e1bd7e6a379b9e0789d5e216edefdec23
madzieboyles <53319246+madzieboyles@users.noreply.github.com>
2022-03-18 15:56:39 -0600

    Update c2.page.ts

--- src/app/computers/c2/c2.page.ts
+++ src/app/computers/c2/c2.page.ts
@@ -131,7 +131,6 @@ this.pledgeSubmittedCard = false;
 }
 
 
-
 checkForPledgeContent(){
   var user = firebase.auth().currentUser;
   if (user != null) {
@@ -141,27 +140,10 @@ let verfCheck = firebase.firestore().collection('userProfile').doc(`${this.uid}`
   this.pledgeContent = docSnapshot.data().c2;
   console.log(this.pledgeContent);
 
-  //NEW CONTENT BELOW HERE
-  if (this.pledgeContent.includes(' ') || this.pledgeContent.includes('.') || this.pledgeContent.includes(',')) {
-    let approvalStatus={
-      c2: "approved"
-    }
-    this.hideVerfCard = true;
-    const approved = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
-    .collection('approval').doc('computers').update(approvalStatus);
-    this.pledgeSubmittedCard = false;
-  }
   if (this.pledgeContent !=''){
-    let approvalStatus = {
-      c2: "pending"
-    }
     this.hideVerfCard = true;
-    const pending = firebase.firestore().collection('userProfile').doc(`${this.uid}`)
-    .collection('approval').doc('computers').update(approvalStatus);
-    this.pledgeSubmittedCard = false;
   }
 })
-// NEW CONTENT ABOVE HERE
 this.checkForPledgeStatus();
 }
 
